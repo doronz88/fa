de368687fb1c17943aae0b126fa5defa4210b375
DoronZ <doron88@gmail.com>
2020-05-19 12:04:11 +0300

    update commands and README

---
+++ fa/commands/function_lines.py
@@ -0,0 +1,23 @@
+from fa.commands import utils
+
+try:
+    import idautils
+except ImportError:
+    pass
+
+
+def get_parser():
+    p = utils.ArgumentParserNoExit()
+    return p
+
+
+@utils.yield_unique
+def function_opcodes(addresses):
+    for address in addresses:
+        for item in idautils.FuncItems(address):
+            yield item
+
+
+def run(segments, args, addresses, **kwargs):
+    utils.verify_ida()
+    return list(function_opcodes(addresses))
