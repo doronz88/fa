d25419826a1084be467c3641a6a10e57c38dda09
DoronZ <doron88@gmail.com>
2020-06-12 18:03:19 +0300

    remove redundant const

--- ida_loader.py
+++ ida_loader.py
@@ -18,7 +18,6 @@ import idc
 from fa import fainterp
 
 TEMP_SIG_FILENAME = os.path.join(tempfile.gettempdir(), 'fa_tmp_sig.sig')
-IS_BE = '>' if idaapi.get_inf_structure().is_be() else '<'
 
 
 def open_file(filename):
