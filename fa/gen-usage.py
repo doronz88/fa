37779d78123da5c88b7a7d8f2c36689eb7b6f2d2
DoronZ <doron88@gmail.com>
2020-06-19 16:18:01 +0300

    gen-usage: fix import path

--- fa/gen-usage.py
+++ fa/gen-usage.py
@@ -1,5 +1,9 @@
-from fa.fainterp import FaInterp
 import os
+import sys
+
+sys.path.append('.')  # noqa: E402
+
+from fa.fainterp import FaInterp
 
 COMMANDS_ROOT = os.path.join(
     os.path.dirname(os.path.abspath(__file__)), 'commands')
@@ -8,7 +12,7 @@ COMMANDS_ROOT = os.path.join(
 def main():
     for filename in os.listdir(COMMANDS_ROOT):
         if filename.endswith('.py') and \
-                filename not in ('utils.py', '__init__.py'):
+                filename not in ('__init__.py',):
             command = os.path.splitext(filename)[0]
             command = FaInterp.get_command(command)
 
