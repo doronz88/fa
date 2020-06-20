70e5ade3cac52d6e18a4ed7e230b2427a461a0df
DoronZ <doron88@gmail.com>
2020-06-20 13:49:04 +0300

    make goto-ref yield unique results

--- fa/commands/goto_ref.py
+++ fa/commands/goto_ref.py
@@ -40,4 +40,4 @@ def goto_ref_unique(addresses, code=False, data=False):
 
 
 def run(segments, args, addresses, interpreter=None, **kwargs):
-    return list(set(goto_ref(addresses, code=args.code, data=args.data)))
+    return list(set(goto_ref_unique(addresses, code=args.code, data=args.data)))
